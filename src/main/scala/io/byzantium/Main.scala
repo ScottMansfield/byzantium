91ad51176794e0c10b5e27e6eab94c496f6c880a
Scott Mansfield <sgmansf@gmail.com>
2015-07-08 20:50:57 -0700

    Cleaning up main

--- src/main/scala/io/byzantium/Main.scala
+++ src/main/scala/io/byzantium/Main.scala
@@ -2,18 +2,11 @@ package io.byzantium
 
 import io.byzantium.importers.eagle.EagleImporter
 
-import scalax.collection.Graph
-import scalax.collection.GraphPredef._
-import scalax.collection.edge.LHyperEdge
-
-// Will want to use LHyperEdge to connect different PCB bits
-// not sure about the negative spaces
-
 /**
  * @author Scott Mansfield
  */
 object Main {
   def main(args: Array[String]) {
-    EagleImporter("src/test/resources/ESP8266_BREAKOUT.brd").read()
+    val brdGraph = EagleImporter("src/test/resources/ESP8266_BREAKOUT.brd").read()
   }
 }
